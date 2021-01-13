94338680d8be38cccca2c260610055e05ae5d35a
Muhammad Hidayat <muhidayat03@gmail.com>
2021-01-13 22:00:29 +0700

    remove unused file

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
-import main from './script/view/main';
-import './script/components/app-bar';
+import main from './script/view/main'; 
 import './script/components/app-header';
-import './script/components/search-bar';
-import './script/components/club-list';
-import './script/components/club-item';
+import './script/components/search-bar'; 
 import './script/components/movie-item';
 import './script/components/movie-list';
 import './script/components/loading';
